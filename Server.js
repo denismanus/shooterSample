cfacb2c85ae41df6c40f3069e90dfde712ba9072
denisman <denis.kozlov@binibambini.com>
2019-04-15 17:51:41 +0300

    first commit

---
+++ Server.js
@@ -0,0 +1,65 @@
+var io = require('socket.io')(process.env.PORT||3000); 
+var shortid = require('shortid');
+
+var players = [];
+var bullets = [];
+
+io.on('connection', function(socket){
+
+    var thisPlayerId = shortid.generate();
+    var thisPlayerTeam = Math.floor(Math.random() * 2);
+    socket.emit('register', { id: thisPlayerId, teamId: thisPlayerTeam});
+    socket.broadcast.emit('spawn', { id: thisPlayerId, teamId: thisPlayerTeam });
+
+    var player = {
+        id: thisPlayerId,
+        teamId: thisPlayerTeam
+    };
+
+    players[thisPlayerId] = player;
+    socket.broadcast.emit('requestPosition');
+    socket.broadcast.emit('requestDestination');
+    
+    for(var playerId in players){
+        
+        if(playerId == thisPlayerId)
+            continue;
+        
+        socket.emit('spawn', players[playerId]);
+        console.log('sending spawn to new player for id: ', playerId);
+    };
+
+    socket.on('move', function(data){
+        //data.id = thisPlayerId;
+        console.log('client moved', data.x)
+        console.log('client id', thisPlayerId)
+        socket.broadcast.emit('move', data);
+    });
+
+    socket.on('shoot', function(data){
+        data.teamId = thisPlayerTeam;
+        socket.broadcast.emit('shoot', data);
+    });
+
+    socket.on('updatePosition', function(data) {
+        console.log("update position: ", data.y);
+        data.id = thisPlayerId;
+        
+        socket.broadcast.emit('updatePosition', data);
+    });
+
+    socket.on('updateDestination', function(data) {
+        data.id = thisPlayerId;
+        
+        socket.broadcast.emit('updateDestination', data);
+    });  
+
+
+    socket.on('disconnect', function(data){
+        console.log('client disconnected');
+        
+        delete players[thisPlayerId];
+        
+        socket.broadcast.emit('disconnected', { id: thisPlayerId });
+	});
+})
